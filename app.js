56ce5bca530d0d58677bf6e5c83dbc120e574d7d
dvidalv <dvidalv@gmail.com>
2023-12-20 18:58:05 -0400

    iniciando el backend

---
+++ app.js
@@ -0,0 +1,12 @@
+//crear un servidor con express
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+		res.send('Hola Mundo');
+});
+
+app.listen(3000, () => {
+		console.log('Servidor iniciado');
+}
